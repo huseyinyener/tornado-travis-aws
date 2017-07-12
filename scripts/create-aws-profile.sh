1f7ecb7bc5496169f030b085c5872f676056d66c
Selçuk Usta <susta@hurriyet.com.tr>
2017-07-12 17:15:43 +0300

    Project is created

---
+++ scripts/create-aws-profile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+AWS_PUBLIC_KEY=$AWS_PUBLIC_KEY
+AWS_PRIVATE_KEY=$AWS_PRIVATE_KEY
+mkdir ~/.aws
+cat > ~/.aws/config <<EOL
+[profile eb-tornado-profile]
+aws_access_key_id = ${AWS_PUBLIC_KEY}
+aws_secret_access_key = ${AWS_PRIVATE_KEY}
+EOL
+
+cd src
+eb init sample-eb-app --region us-west-2 --profile eb-tornado-profile
+eb deploy

fee5e911663a2af0f08ac6d8a86c47e07d981e38
Selçuk Usta <susta@hurriyet.com.tr>
2017-07-12 21:28:27 +0300

    aws script is modified

--- scripts/create-aws-profile.sh
+++ scripts/create-aws-profile.sh
@@ -10,5 +10,5 @@ aws_secret_access_key = ${AWS_PRIVATE_KEY}
 EOL
 
 cd src
-eb init [BEANSTALK_APPLICATION_NAME_WILL_BE_HERE] --region us-west-2 --profile eb-tornado-profile
+eb init tornado-sample-app --region us-west-2 --profile eb-tornado-profile
 eb deploy
